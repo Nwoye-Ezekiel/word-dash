485ae48f435f2344e23e15ce60b4d08ab0bc9b02
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2022-08-21 14:11:20 +0100

    fix logic issue

--- src/components/play/index.tsx
+++ src/components/play/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useCountdown } from 'usehooks-ts'
+import { useCountdown } from "usehooks-ts";
 import styles from "./index.module.css";
 import { fetchRandomQuote } from "../../apis";
 import SetupModal from "../modals/setup-modal";
@@ -8,33 +8,37 @@ import Button from "../button";
 import Spacer from "../spacer";
 
 export default function Play() {
-  const DEFAULT_TIME = 30
+  const DEFAULT_TIME = 30;
   const [customWords, setCustomWords] = useState("");
   const [displayedWords, setDisplayedWords] = useState(Array<string>);
-  const [displayedWordIndex, setDisplayedWordIndex] = useState(0)
-  const [typedCharacters, setTypedCharacters] = useState("")
-  const [typedCharacterIndex, setTypedCharacterIndex] = useState(-1)
-  const [status, setStatus] = useState("waiting")
-  const [correctScore, setCorrectScore] = useState(0)
-  const [incorrectScore, setIncorrectScore] = useState(0)
-  const [error, setError] = useState(false)
-  const [setupModal, setSetupModal] = useState(false)
-  const [completionModal, setCompletionModal] = useState(false)
-  const [inputType, setInputType] = useState<'space' | 'backspace' | 'text'>('text')
-  const [timer, setTimer] = useState(DEFAULT_TIME)
+  const [displayedWordIndex, setDisplayedWordIndex] = useState(0);
+  const [typedCharacters, setTypedCharacters] = useState("");
+  const [status, setStatus] = useState("waiting");
+  const [correctScore, setCorrectScore] = useState(0);
+  const [incorrectScore, setIncorrectScore] = useState(0);
+  const [error, setError] = useState(false);
+  const [setupModal, setSetupModal] = useState(false);
+  const [completionModal, setCompletionModal] = useState(false);
+  const [inputType, setInputType] = useState<"space" | "backspace" | "text">(
+    "text"
+  );
+  const [timer, setTimer] = useState(DEFAULT_TIME);
   const [count, { startCountdown, stopCountdown, resetCountdown }] =
     useCountdown({
       countStart: timer,
       intervalMs: 1000,
-    })
-  const textInput = useRef<HTMLTextAreaElement>(null)
-  
+    });
+
+  const textInput = useRef<HTMLTextAreaElement>(null);
+
   useEffect(() => {
     generateNewQuote();
   }, []);
 
   useEffect(() => {
     if (status === "started") textInput.current?.focus();
+    // setTimeout(() => { window.scroll(0,0) }, 500);
+    // window.scrollTo(0, 0);
   }, [status]);
 
   useEffect(() => {
@@ -45,7 +49,7 @@ export default function Play() {
     if (count === 0) {
       stopCountdown();
       setCompletionModal(true);
-      setStatus('finished');
+      setStatus("finished");
     }
   }, [count]);
 
@@ -72,11 +76,11 @@ export default function Play() {
   function generateStyleClass(wordIndex: number, characterIndex: number) {
     if (wordIndex < displayedWordIndex) return "correct";
     else if (wordIndex === displayedWordIndex) {
-      if (characterIndex < typedCharacterIndex + 1) {
+      if (characterIndex < typedCharacters.length) {
         if (
           displayedWords[displayedWordIndex][characterIndex] ===
             typedCharacters[characterIndex] &&
-          !(typedCharacterIndex === displayedWords[displayedWordIndex].length)
+          !(typedCharacters.length > displayedWords[displayedWordIndex].length)
         ) {
           return "correct";
         } else {
@@ -91,41 +95,44 @@ export default function Play() {
   }
 
   const handleInputChange = (value: string) => {
+    value = value
+      .replace("”", '"')
+      .replace("“", '"')
+      .replace("’", "'")
+      .replace("‘", "'")
+      .replace(/[\u2018\u2019]/g, "'")
+      .replace(/[\u201C\u201D]/g, '"')
+      .replace(/[\u2013\u2014]/g, "-")
+      .replace(/[\u2026]/g, "...");
+
     setTypedCharacters(value);
+
     if (
       displayedWordIndex + 1 === displayedWords.length &&
       value === displayedWords[displayedWordIndex]
     ) {
-      !error && setCorrectScore((prevScore) => prevScore + 1 );
+      !error && setCorrectScore((prevScore) => prevScore + 1);
       setDisplayedWordIndex(displayedWordIndex + 1);
       stopCountdown();
       setCompletionModal(true);
-      setStatus('finished');
-    }
-    else if (inputType === "space") {
-      if (typedCharacters !== displayedWords[displayedWordIndex]) {
-        setTypedCharacterIndex(typedCharacterIndex + 1);
-      } else {
-        !error && setCorrectScore(correctScore + 1);
-        setTypedCharacters("");
-        setDisplayedWordIndex(displayedWordIndex + 1);
-        setTypedCharacterIndex(-1);
-        setError(false);
-      }
-    } else if (inputType === "backspace") {
-      setTypedCharacterIndex(typedCharacterIndex - 1);
-    } else {
-      setTypedCharacterIndex(typedCharacterIndex + 1);
+      setStatus("finished");
+    } else if (
+      inputType === "space" &&
+      typedCharacters === displayedWords[displayedWordIndex]
+    ) {
+      !error && setCorrectScore(correctScore + 1);
+      setTypedCharacters("");
+      setDisplayedWordIndex(displayedWordIndex + 1);
+      setError(false);
     }
   };
 
   const resetGame = () => {
-    setStatus('waiting');
+    setStatus("waiting");
     resetCountdown();
     generateNewQuote();
     setDisplayedWordIndex(0);
     setTypedCharacters("");
-    setTypedCharacterIndex(-1);
     setError(false);
     setCorrectScore(0);
     setIncorrectScore(0);
@@ -144,13 +151,12 @@ export default function Play() {
     resetCountdown();
     setDisplayedWordIndex(0);
     setTypedCharacters("");
-    setTypedCharacterIndex(-1);
     setError(false);
     setCorrectScore(0);
     setIncorrectScore(0);
     setInputType("text");
-  }
- 
+  };
+
   return (
     <div className={styles["main-container"]}>
       <p
@@ -209,17 +215,18 @@ export default function Play() {
           className={styles["input-display"]}
           onKeyDown={(e) => handleKeyDown(e)}
           maxLength={displayedWords[displayedWordIndex]?.length + 1}
-          disabled={status === "waiting" || status === "finished"}
+          disabled={
+            status === "waiting" ||
+            status === "finished" ||
+            status === "stopped"
+          }
           autoComplete="off"
         />
       </div>
       <div className={styles["action-buttons"]}>
         {status !== "started" && (
           <>
-            <Button
-              variant="outline"
-              onClick={() => setSetupModal(true)}
-            >
+            <Button variant="outline" onClick={() => setSetupModal(true)}>
               Setup
             </Button>
             <Spacer width={30} />
@@ -228,10 +235,11 @@ export default function Play() {
             </Button>
           </>
         )}
-        {status === "started" && 
+        {status === "started" && (
           <Button onClick={stopGame} variant="solid">
-              Stop
-          </Button>}
+            Stop
+          </Button>
+        )}
       </div>
       {setupModal && (
         <SetupModal
