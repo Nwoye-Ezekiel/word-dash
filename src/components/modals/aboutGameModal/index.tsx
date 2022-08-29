aa06deea0306ec8ae89cac756e49a394e90ea36f
Nwoye Ezekiel <57937355+Nwoye-Ezekiel@users.noreply.github.com>
2022-08-29 02:00:42 +0100

    edit about game info

--- src/components/modals/aboutGameModal/index.tsx
+++ src/components/modals/aboutGameModal/index.tsx
@@ -12,7 +12,7 @@ export default function AboutGameModal({ close }: { close: () => void }) {
         </p>
         <p>
           In order to complete the game, the player must quickly type in words
-          with accuracy to avoid losing points.
+          accurately to avoid losing points.
         </p>
         <p>
           This game serves as both a challenge and a way to improve one&apos;s
