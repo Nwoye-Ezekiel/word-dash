d3a719b12d80b32b3c7edbfdc15bf876b57d2bb4
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2022-08-30 06:45:57 +0100

    fix ui issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <div>
       <Head>
         <title>Word Dash</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="preload" as="image" href="../assets/images/liquid.svg" />
         <link rel="preload" as="image" href="../../assets/images/logo1.svg" />
       </Head>
